df3ead6b21c622693fcc6b8170cc014a4e282c82
Unknown <provincerice@gmail.com>
2020-02-23 09:06:08 -0700

    added GUI

--- Gander.py
+++ Gander.py
@@ -4,7 +4,11 @@ import pigpio
 import tweepy
 import face_recognition
 import os
+from tkinter import Tk, Label, Button, Entry, StringVar
+from functools import partial
 
+
+handle_input = ""
 
 # Constants
 SERVO_SPEED = 10
@@ -21,6 +25,23 @@ pi = pigpio.pi()
 # set to middle
 position = 1500
 pi.set_servo_pulsewidth(18, position) # middle
+
+def need_move_servo(face_x, face_width, img):
+    face_center = face_x + (face_width/2)
+    img_center = img.shape[1] / 2
+    THRESHOLD = face_width/4
+
+    if img_center - face_center > THRESHOLD:
+        return -1
+    elif face_center - img_center > THRESHOLD:
+        return 1
+    else:
+        return 0
+
+def add_and_post(tk, text):
+    handle_input = text.get()
+    tk.destroy
+
 
 # Twitter API
 twitter_auth_keys = {
@@ -45,9 +66,19 @@ path = "./Photos/"
 known_faces=[]
 names = {}
 last_index = 0
+
+# GUI
+top = Tk()
+top.geometry("400x250")
+handle_label = Label(top, text = "Handle").place(x = 30,y = 50)
+stringinput = StringVar()
+sbmitbtn = Button(top, text = "Submit",activebackground = "pink", activeforeground = "blue").place(x = 30, y = 170)
+e1 = Entry(top, textvariable=stringinput).place(x = 80, y = 50)
+add_and_post = partial(top, stringinput)
+
 try:
     for index, image_name in enumerate(os.listdir(path)):
-        #filenam
+        #filename
         #print(image_path)
         person_image = face_recognition.load_image_file(path + image_name)
         known_faces.append( face_recognition.face_encodings(person_image)[0])
@@ -57,18 +88,6 @@ try:
 except IndexError:
     print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
     quit()
-
-def need_move_servo(face_x, face_width, img):
-    face_center = face_x + (face_width/2)
-    img_center = img.shape[1] / 2
-    THRESHOLD = face_width/4
-
-    if img_center - face_center > THRESHOLD:
-        return -1
-    elif face_center - img_center > THRESHOLD:
-        return 1
-    else:
-        return 0
 
 # loop runs if capturing has been initialized. 
 while 1:
@@ -81,8 +100,14 @@ while 1:
 
     # Detects faces of different sizes in the input image 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    face_cords = [0,0,0,0]
 
     for (x, y, w, h) in faces:
+        # Save cords
+        face_cords[0] = x
+        face_cords[1] = y
+        face_cords[2] = w
+        face_cords[3] = h
         # To draw a rectangle in a face  
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
@@ -114,14 +139,15 @@ while 1:
     if k == 27:
         # escape pressed
         break
-    elif k == 8:
+    elif k == 8 and len(faces) > 0:
         # back space pressed
 
-        # Take Picture
+        # Take Pictures
         cv2.imwrite('upload.png', img)
+        cv2.imwrite('crop.png', img.crop(face_cords[0], face_cords[1], face_cords[2], face_cords[3]))
 
         # Check if face is in DB
-        unknown_image = face_recognition.load_image_file("upload.png")
+        unknown_image = face_recognition.load_image_file("crop.png")
         unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
         # results is an array of True/False telling if the unknown face matched anyone in the known_faces array
         results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
@@ -129,11 +155,12 @@ while 1:
         if not True in results:
             input_good = False
             while input_good == False:
-                handle = input("What is your twitter handle: ")
-                if " " not in handle and "@" not in handle and handle != "":
+                top.mainloop()
+                handle = handle_input
+                if " " not in handle and "@" not in handle and "#" not in handle and handle != "":
                     input_good = True
             image_name = handle + ".png"
-            os.system("cp upload.png ./Photos/" + image_name)
+            os.system("cp crop.png ./Photos/" + image_name)
             person_image = face_recognition.load_image_file(path + image_name)
             known_faces.append(face_recognition.face_encodings(person_image)[0])
             filename = os.path.splitext(image_name)[0]  # removes .jpg from name to store in dictionary
